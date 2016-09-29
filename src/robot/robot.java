65fe5d62a15087a9160baa5b042921cc1cd98326
gfike19 <gfike19@gmail.com>
2016-09-29 18:23:52 -0500

    edited this based on class lecture

--- src/robot/robot.java
+++ src/robot/robot.java
@@ -7,43 +7,49 @@ private int y_pos;
 private int speed;
 private String dir;
 
-public robot () {
-	this.name =  "";
-	this.x_pos =  0;
-	this.y_pos  = 0;
-	this.speed = 0;
-	this.dir  = "";
+public robot (String name, int x_pos, int y_pos, int speed, String dir) {
+//	this.name =  "";
+//	this.x_pos =  0;
+//	this.y_pos  = 0;
+//	this.speed = 0;
+//	this.dir  = "";
 	
-	public static String move (x_pos, y_pos, speed, dir) {
-		dir = dir.equalsIgnoreCase(dir);
-		
-		switch(dir) {
-		case north:
-			y_pos = (y_pos + 1) * speed;
-		case south: 
-			y_pos = (y_pos - 1) * speed;
-		case east:
-			x_pos = (x_pos + 1) * speed;
-		case west:
-			x_pos = (x_pos - 1) * speed;
+	public String getName () {
+		return this.name;
 	}
-		return (x_pos + "" + y_pos);
-	}
-	
-	public static String rotate(boolean rot) {
-		dir = dir.equalsIgnoreCase(dir);
-		String dir_list [] = {
-				"north", "east", "south", "west"
-		};
-		if(rot == true) {
-			String current_dir = dir_list.find(dir);
-			dir = current_dir +1;
-		} return dir;
-	}
-	
-	public static int dist () {
-		
-	}return dist;
-	
+
 }
+
+public static String move (x_pos, y_pos, speed, dir) {
+	dir = dir.equalsIgnoreCase(dir);
+	
+	switch(dir) {
+	case north:
+		y_pos = (y_pos + 1) * speed;
+	case south: 
+		y_pos = (y_pos - 1) * speed;
+	case east:
+		x_pos = (x_pos + 1) * speed;
+	case west:
+		x_pos = (x_pos - 1) * speed;
+}
+	return (x_pos + "" + y_pos);
+}
+
+public static String rotate(boolean rot) {
+	dir = dir.equalsIgnoreCase(dir);
+	String dir_list [] = {
+			"north", "east", "south", "west"
+	};
+	if(rot == true) {
+		String current_dir = dir_list.find(dir);
+		dir = current_dir +1;
+	} return dir;
+}
+
+public static int dist () {
+	
+}return dist;
+
+
 }
